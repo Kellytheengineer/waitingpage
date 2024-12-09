47e032f199f551e14f2e993da55c3c4e002effb3
Kellytheengineer <77806498+Kellytheengineer@users.noreply.github.com>
2024-12-09 02:37:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-document.getElementById('subscribeForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const name = document.getElementById('name').value;
 
-    fetch('https://hooks.zapier.com/hooks/catch/20911541/2iqxb1k/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email: email,
-            name: name
-        })
-    })
-    .then(response => {
-        alert('Successfully subscribed!');
-        this.reset();
-    })
-    .catch(error => {
-        alert('Subscription failed. Please try again.');
-    });
-});
 
